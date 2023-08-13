02128db452f14a09aca10750c730a4f2def9cc93
Romain <romain.granai@gmail.com>
2023-08-13 15:18:20 +0200

    topbar

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+    console.log('There will be blood');
+
+    topbar();
+
+    function topbar(){
+        console.log('topbar')
+    }
+
+});
---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+//@prepros-append vendor/jquery.js
+//@prepros-append vendor/gsap.js
+//@prepros-append vendor/ScrollTrigger.js
+//@prepros-append vendor/scrollToPlugin.js
+//@prepros-append vendor/imagesloaded.js
+//@prepros-append vendor/swiper.js
+//@prepros-append vendor/lenis.js
+
+//@prepros-append app.js
