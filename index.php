23a7f604ba40b9c725f62d7a073fc58be331f974
Romain <romain.granai@gmail.com>
2023-08-13 12:41:23 +0200

    init

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="">
+
+<head>
+  <meta charset="utf-8">
+    <title>Mangia!</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/main.min.css">
+    <meta name="theme-color" content="FFD12D">
+</head>
+
+<body>
+  <header class="header header--home"></header>
+  <p>Hello world! This is HTML5 Boilerplate.</p>
+  <script src="<?php echo get_template_directory_uri(); ?>/assets/js/main.min.js"></script>
+
+</body>
+
+</html>
